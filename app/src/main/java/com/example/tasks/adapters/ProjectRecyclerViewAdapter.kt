d7028bd7c52dc73fb001fa4f5975e8ac8620be72
Jude Francis <judecodes@gmail.com>
2020-12-18 00:07:54 +0100

    feat: show projects list
    - implement recycler view adapter & list item layout

---
+++ app/src/main/java/com/example/tasks/adapters/ProjectRecyclerViewAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.tasks.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tasks.data.db.entities.Project
+import com.example.tasks.databinding.ListItemProjectsBinding
+
+class ProjectRecyclerViewAdapter : RecyclerView.Adapter<ProjectRecyclerViewAdapter.ViewHolder>() {
+    inner class ViewHolder(
+        private val binding: ListItemProjectsBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(project: Project) {
+            binding.name.text = project.name
+            binding.root.setOnClickListener {
+                onItemClickListener?.let { it(project) }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemProjectsBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        return holder.bind(differ.currentList[position])
+    }
+
+    override fun getItemCount() = differ.currentList.size
+
+    private val callback = object : DiffUtil.ItemCallback<Project>() {
+        override fun areItemsTheSame(old: Project, new: Project) = old.id == new.id
+
+        override fun areContentsTheSame(old: Project, new: Project) = old == new
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    private var onItemClickListener: ((Project) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Project) -> Unit) {
+        onItemClickListener = listener
+    }
+}
--- app/src/main/java/com/example/tasks/ui/projects/ProjectsFragment.kt
+++ app/src/main/java/com/example/tasks/ui/projects/ProjectsFragment.kt
@@ -2,11 +2,17 @@ package com.example.tasks.ui.projects
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tasks.R
+import com.example.tasks.adapters.ProjectRecyclerViewAdapter
 import com.example.tasks.databinding.FragmentProjectsBinding
+import com.example.tasks.ui.MainActivity
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import timber.log.Timber
@@ -14,20 +20,35 @@ import timber.log.Timber
 @AndroidEntryPoint
 class ProjectsFragment : Fragment(R.layout.fragment_projects) {
 
+    private lateinit var projectsAdapter: ProjectRecyclerViewAdapter
     private val viewModel: ProjectsViewModel by viewModels()
     private lateinit var binding: FragmentProjectsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProjectsBinding.bind(view)
+        (requireActivity() as MainActivity).supportActionBar?.title = "Projects"
         initialize()
     }
 
     private fun initialize() {
         lifecycleScope.launchWhenStarted {
             viewModel.projects.collect {
-                Timber.d("Projects $it")
+                projectsAdapter.differ.submitList(it)
             }
+        }
+        binding.rvProjects.apply {
+            projectsAdapter = ProjectRecyclerViewAdapter()
+            projectsAdapter.setOnItemClickListener {
+                Snackbar.make(requireView(), it.name, Snackbar.LENGTH_SHORT)
+                    .setBackgroundTint(
+                        ContextCompat.getColor(requireContext(), R.color.popupBackground)
+                    )
+                    .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                    .show()
+            }
+            adapter = projectsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
         }
     }
 }
